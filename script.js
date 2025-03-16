9c7743ffa7be9e61ef46328dafe1654af39a831f
DDoudine <amandinekemp@gmail.com>
2025-03-16 20:18:30 +0100

    Change back url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function getRandomJoke() {
-    const response = await fetch("http://localhost:3000/api/jokes/random");
+    const response = await fetch("http://localhost:3000/blagues/random");
     const data = await response.json();
     document.getElementById("joke").textContent = data.content ? data.content : "Aucune blague trouvée";
 }
