5e447d0ab7071718af483a6f7718317da5f2db1b
DDoudine <amandinekemp@gmail.com>
2025-03-16 22:21:40 +0100

    Display answer

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 async function getRandomJoke() {
     const response = await fetch("http://localhost:3000/blagues/random");
     const data = await response.json();
-    document.getElementById("joke").textContent = data.content ? data.content : "Aucune blague trouvée";
+    let jokeContent;
+    if (data.content) {
+        jokeContent = data.content + "<br><br>Réponse: " + data.answer;
+    } else {
+        jokeContent = "Aucune blague trouvée";
+    }
+    document.getElementById("joke").innerHTML = jokeContent;
 }
